888db32cf9d638d4fcedddf512b938b998fe8ff8
Lucy20209060 <550502661@qq.com>
2018-09-12 16:58:45 +0800

    feat(page): 无人值守型机器人

--- src/app/client-monitor/robot-unattended/robot-unattended.component.ts
+++ src/app/client-monitor/robot-unattended/robot-unattended.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { delay } from '../../../shared/utils/index'
+import { MyHttpService } from '../../../shared/services';
 
 @Component({
   selector: 'app-robot-unattended',
@@ -7,25 +8,44 @@ import { delay } from '../../../shared/utils/index'
   styleUrls: ['./robot-unattended.component.scss']
 })
 export class RobotUnattendedComponent implements OnInit {
-  conditionInfo:Array<object>;
-  constructor() {
-    this.conditionInfo = [
-      {
-        type: 'Input',
-        key: '机器人昵称',
-        value: '',
-        placeholder: '请输入机器人昵称'
-      }
-    ]
-  }
+
+  conditionInfo:Array<object> = [{ type: 'Input', key: '机器人昵称', value: '', placeholder: '请输入机器人昵称'}]
+  pager:Object = { currentPage: 1, total: 0, pageSize: 0 }
+  tableList:Array<object> = []
+
+  constructor(
+    private myHttp: MyHttpService
+  ) {}
 
   ngOnInit() {
+    this.getTableList();
   }
 
+  // 筛选条件变化
   onChange() {
     delay(()=>{
       console.log('value',this.conditionInfo[0]['value'])
     })
+  }
+
+  // 获取表格数据
+  getTableList() {
+    const { pager } = this;
+    const data = {
+      clientTypeCd: 'robot_unattended',
+      name: this.conditionInfo[0]['value'],
+      currentPage: pager['currentPage'],
+      pageSize: pager['pageSize']
+    }
+    this.myHttp.ajax('/rpa/client/list',data,'get',(res)=>{
+      this.tableList = res.data;
+      this.pager = res.pager;
+    });
+  }
+
+  // 分页
+  paginationChange() {
+    this.getTableList();
   }
 
 }
